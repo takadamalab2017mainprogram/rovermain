59299193b7351ce9934581247b333fd8acbc0f7b
takahiroJinba <takahiro412.c@gmail.com>
2014-07-23 22:32:47 +0900

    パラ検知: 回避後にしばらく直進するためのモータ出力を忘れてたので追加

--- sequence.cpp
+++ sequence.cpp
@@ -379,6 +379,7 @@ void Separating::onUpdate(const struct timespec& time)
 		if(!gTurningState.isActive())
 		{
 			Debug::print(LOG_SUMMARY, "Para check: Turn Finished!\r\n");
+			gMotorDrive.drive(100,100);
 			mLastUpdateTime = time;
 			mCurStep = STEP_GO_FORWARD;
 		}
@@ -386,6 +387,7 @@ void Separating::onUpdate(const struct timespec& time)
 	case STEP_GO_FORWARD:	//パラ検知後，しばらく直進する
 		if(Time::dt(time,mLastUpdateTime) > 3)
 		{
+			gMotorDrive.drive(0,0);
 			nextState();
 		}
 		break;
