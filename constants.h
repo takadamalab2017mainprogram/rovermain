ac4040e770bd162e8e68f4e2e9bcdd61bd3ce87c
f1410115@edu.cc.uec.ac.jp <f1410115@edu.cc.uec.ac.jp>
2017-06-29 21:13:21 +0900

    LED

--- constants.h
+++ constants.h
@@ -27,12 +27,15 @@ const static int PIN_PWM_B2 = 29;                              //Clear!
 const static int PIN_BUZZER = 12;//ブザー                               Clear!
 const static int PIN_XBEE_SLEEP = 13;//XBeeスリープピン
 const static int PIN_LIGHT_SENSOR = 14;//Cdsセンサピン                  Clear!
-const static int PIN_PARA_SERVO = 23;// パラシュートサーボピン          Clear!
-const static int PIN_JOHN_SERVO = 25;//                                 Clear!
-const static int PIN_MIKE_SERVO = 21;       //スタビサーボピン          Clear!
-const static int PIN_NECK_SERVO = 1;//                                  Clear!
-const static int PIN_ARM_SERVO = 22;//                                  Clear!
+const static int PIN_MULTI_SERVO = 1;//パラ＋バックスタビ          Clear!
+//const static int PIN_JOHN_SERVO = 25;//                                 Clear!
+//const static int PIN_MIKE_SERVO = ;       //スタビサーボピン          Clear!
+//const static int PIN_NECK_SERVO = 1;//                                  Clear!
+//const static int PIN_ARM_SERVO = 22;//                                  Clear!
 const static int PIN_DISTANCE = 8;//距離センサー(ピン番号は適当)
+const static int PIN_LED_R = 24;// LED
+const static int PIN_LED_G = 22;// LED
+const static int PIN_LED_B = 23;// LED
 
 //モータ設定
 const static int MOTOR_MAX_POWER = 100;
