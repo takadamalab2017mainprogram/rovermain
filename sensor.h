65e2dacb2c58a9f56d2fd623c4211a994a44ad81
Yamato Murakami <yoyoyoyamato@gmail.com>
2017-07-04 22:31:27 +0900

    fix sensor.h

--- sensor.h
+++ sensor.h
@@ -166,7 +166,7 @@ public:
 	double getTheta() const; //XY
 	double getPsi() const; //YZ
 	double getPhi() const; //XZ
-	
+
 	bool getRawAccel(VECTOR3& acc) const;
 
 	AccelerationSensor();
@@ -268,6 +268,56 @@ public:
 	~CameraCapture();
 };
 */
+class NineAxisSensor : public TaskBase
+{
+private:
+	int mFileHandle;
+	VECTOR3 mAccel, mAccelAve;
+	VECTOR3 mRVel, mRAngle;
+	VECTOR3 mMagnet;
+	struct timespec mLastSampleTime;
+	std::list<VECTOR3> mRVelHistory;
+	VECTOR3 mRVelOffset;
+protected:
+	virtual bool onInit(const struct timespec& time);
+	virtual void onClean();
+	virtual void onUpdate(const struct timespec& time);
+	virtual bool onCommand(const std::vector<std::string>& args);
+public:
+	bool getAccel(VECTOR3& acc) const;
+	double getAx() const;
+	double getAy() const;
+	double getAz() const;
+
+	double getTheta() const; //XY
+	double getPsi() const; //YZ
+	double getPhi() const; //XZ
+
+	//現在の角度を返す(-180〜+180)
+	bool getRPos(VECTOR3& pos) const;
+	double getRx() const;
+	double getRy() const;
+	double getRz() const;
+
+	bool getRVel(VECTOR3& vel) const;
+	double getRvx() const;
+	double getRvy() const;
+	double getRvz() const;
+
+	//現在の角度を基準とする
+	void setZero();
+
+	//引数のベクトルを(-180〜+180)の範囲に修正
+	static void normalize(VECTOR3& pos);
+	static double normalize(double pos);
+
+	bool getMagnet(VECTOR3& mag);
+	double getMx() const;
+	double getMy() const;
+	double getMz() const;
+	NineAxisSensor();
+	~NineAxisSensor();
+};
 extern GyroSensor gGyroSensor;
 extern GPSSensor gGPSSensor;
 extern PressureSensor gPressureSensor;
@@ -276,4 +326,3 @@ extern LightSensor gLightSensor;
 extern DistanceSensor gDistanceSensor;
 //extern CameraCapture gCameraCapture;
 extern AccelerationSensor gAccelerationSensor;
-
