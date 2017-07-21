79c18c317056effc6fcabaa52f7fe9c94b0768bd
f1410115@edu.cc.uec.ac.jp <f1410115@edu.cc.uec.ac.jp>
2017-07-21 22:45:03 +0900

    関数化をやめてコマンド内に記述

--- chat.cpp
+++ chat.cpp
@@ -32,7 +32,43 @@ bool Send::onCommand(const vector<string>& args)
 	{
 		if (args[1].compare("sen") == 0)
 		{	
-			send();
+			//soket作成時のエラーを表示
+			if ((sock1 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+			{
+				perror("socket");
+				exit(1);
+			}
+			memset((char*)&addr, 0, sizeof(addr));
+			addr.sin_family = AF_INET;
+			addr.sin_port = htons(12345);
+			addr.sin_addr.s_addr = INADDR_ANY;
+			//bindが残ってしまう仕様を変更、すぐに接続できるようになる
+			setsockopt(sock1, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
+			//bind時のエラー
+			if (bind(sock1, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+			{
+				perror("bind");    exit(1);
+			}
+			//listenのエラーを表示
+			if (listen(sock1, 5) < 0)
+			{
+				perror("listen"); exit(1);
+			}
+
+			len = sizeof(client);
+			if (sock2 = accept(sock1, (struct sockaddr *)&client, (socklen_t *)&len) < 0)
+			{
+				perror("accept"); exit(1);
+			}
+			else
+			{
+				//相手のIPアドレスとポート番号を表示
+				Debug::print(LOG_PRINT, "accepted connection from %s, port=%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+			}
+			close(sock1);
+			strcpy(buf, "I'm a server.\n");
+			write(sock2, buf, sizeof(buf));
+			close(sock2);
 			return true;
 		}
 		return false;
@@ -46,43 +82,7 @@ chat_s sen: send messeage to client\r\n");
 
 bool Send::send()
 {
-	//soket作成時のエラーを表示
-	if ((sock1 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket");
-		exit(1);
-	}
-	memset((char*)&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
-	addr.sin_addr.s_addr = INADDR_ANY;
-	//bindが残ってしまう仕様を変更、すぐに接続できるようになる
-	setsockopt(sock1, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
-	//bind時のエラー
-	if (bind(sock1, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	{
-		perror("bind");    exit(1);
-	}
-	//listenのエラーを表示
-	if (listen(sock1, 5) < 0)
-	{
-		perror("listen"); exit(1);
-	}
-
-	len = sizeof(client);
-	if (sock2 = accept(sock1, (struct sockaddr *)&client, (socklen_t *)&len) < 0)
-	{
-		perror("accept"); exit(1);
-	}
-	else
-	{
-		//相手のIPアドレスとポート番号を表示
-		Debug::print(LOG_PRINT, "accepted connection from %s, port=%d\n",inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-	}
-		close(sock1);
-		strcpy(buf, "I'm a server.\n");
-		write(sock2, buf, sizeof(buf));
-		close(sock2);
+	
 		return true;
 }
 
@@ -115,7 +115,34 @@ bool Rec::onCommand(const std::vector<std::string>& args)
 	{
 		if (args[1].compare("rec") == 0)
 		{
-			receive();
+			if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+			{
+				perror("socket"); exit(1);
+			}
+			memset((char*)&server, 0, sizeof(server));
+
+
+			server.sin_family = AF_INET;
+			server.sin_port = htons(12345);
+			server.sin_addr.s_addr = inet_addr("10.0.0.12");
+			if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+			{
+				perror("connect"); exit(1);
+			}
+			n = read(sock, buf, sizeof(buf));
+			if (n > 0)
+			{
+				printf("ソケットからデータを読み取った");
+			}
+			else if (n < 0)
+			{
+				perror("read");
+				printf("何も送られてないです。sendプログラムを実行してください");
+			}
+			else if (n == 0)
+			{
+				Debug::print(LOG_PRINT, "%d,%s\n", n, buf);
+				close(sock);
 			return true;
 		}
 		//Debug::print(LOG_PRINT, "FIREF");
@@ -130,37 +157,7 @@ chat_r rec: recieve message from server\r\n");
 
 bool Rec::receive()
 {
-
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket"); exit(1);
-	}
-	memset((char*)&server, 0, sizeof(server));
-
-
-	server.sin_family = AF_INET;
-	server.sin_port = htons(12345);
-	server.sin_addr.s_addr = inet_addr("10.0.0.12");
-	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-	{
-		perror("connect"); exit(1);
-	}
-	n = read(sock, buf, sizeof(buf));
-	if(n > 0)
-	{
-		printf("ソケットからデータを読み取った");
-	} 
-	else if (n < 0) 
-	{
-		perror("read");
-		printf("何も送られてないです。sendプログラムを実行してください");
-	}
-	else if (n == 0)
-	{
-	Debug::print(LOG_PRINT, "%d,%s\n", n, buf);
-	close(sock);
 	return true;
-	}
 }
 
 Rec::Rec() :buf(), n(0)
