76eb6d52d38b230518b0bf2a194240e6dd904d91
ニワトリ <niwatorihayate@gmail.com>
2017-08-10 18:38:11 +0900

    last time compiler finished. this time clean code.

--- subsidiary_sequence.cpp
+++ subsidiary_sequence.cpp
@@ -16,15 +16,7 @@ Escaping gEscapingState;
 EscapingRandom gEscapingRandomState;
 EscapingByStabi gEscapingByStabiState;
 Waking gWakingState;
-//WakingFromLie gWakingFromLieState;
-//Turning gTurningState;
-//Avoiding gAvoidingState;
-//WadachiPredicting gPredictingState;
-//PictureTaking gPictureTakingState;
 SensorLogging gSensorLoggingState;
-//MovementLogging gMovementLoggingState;
-//EncoderMonitoring gEncoderMonitoringState;
-//CameraSave_Sequence gCameraSave_Sequence;
 
 
 bool Escaping::onInit(const struct timespec& time)
@@ -83,21 +75,10 @@ void Escaping::onUpdate(const struct timespec& time)
        
        
        gMotorDrive.drive(0);
-       //gCameraCapture.startWarming();
      }
    break;
    
  case STEP_CAMERA_TURN:
-   //�摜�����̌��ʁA���]�����K�v���������ꍇ
-	 /*
-		if (Time::dt(time, mLastUpdateTime) > 0.4 || abs(gGyroSensor.getRz() - mAngle) > 70)
-		  {
-		    //gCameraCapture.startWarming();
-		    mCurStep = STEP_CAMERA_FORWARD;
-		    gMotorDrive.drivePIDGyro(0, 100, true);
-		    mLastUpdateTime = time;
-		  }
-		  */
 		break;
  case STEP_CAMERA_FORWARD:
 		//�摜�����̌��ʁA���i�����K�v���������ꍇ
@@ -109,33 +90,6 @@ void Escaping::onUpdate(const struct timespec& time)
 		}
 		break;
 	case STEP_CAMERA_TURN_HERE:
-		//�摜�����̌��ʁA���̏����]�����K�v���������ꍇ
-		/*
-		if (Time::dt(time, mLastUpdateTime) > 0.4 || abs(gGyroSensor.getRz() - mAngle) > 70)
-		{
-		  //gCameraCapture.startWarming();
-			mCurStep = STEP_BACKWARD;
-			gMotorDrive.drive(-100);
-			mLastUpdateTime = time;
-		}
-		break;
-	case STEP_RANDOM:
-		//�����_������
-		if (Time::dt(time, mLastUpdateTime) >= 5)
-		{
-			++mEscapingTriedCount;
-			if (mEscapingTriedCount > ESCAPING_MAX_RANDOM_ESCAPING_COUNT)
-			{
-				//�����_���ڍs
-				mEscapingTriedCount = 0;
-				mCurStep = STEP_BACKWARD;
-				break;
-			}
-			stuckMoveRandom();
-			mLastUpdateTime = time;
-
-		}
-		*/
 		break;
 	}
 }
@@ -255,16 +209,6 @@ void EscapingRandom::onUpdate(const struct timespec& time)
 {
 	switch (mCurStep)
 	{
-		//case STEP_BACKWARD:
-		//	//�o�b�N���s��
-		//	if(Time::dt(time,mLastUpdateTime) >= 3)
-		//	{
-		//		mCurStep = STEP_TURN;
-		//		mLastUpdateTime = time;
-		//		gMotorDrive.drive(100,-100);
-		//		gMultiServo.start(0);					//�X�^�r������
-		//	}
-		//	break;
 	case STEP_TURN:
 		//���̏����]���s��
 		if (Time::dt(time, mLastUpdateTime) >= 3)
@@ -272,7 +216,6 @@ void EscapingRandom::onUpdate(const struct timespec& time)
 			mCurStep = STEP_FORWARD;
 			mLastUpdateTime = time;
 			gMotorDrive.drive(100);
-			//gMultiServo.start(STABI_BASE_ANGLE);	//�X�^�r�L�΂�
 		}
 		break;
 	case STEP_FORWARD:
@@ -282,7 +225,6 @@ void EscapingRandom::onUpdate(const struct timespec& time)
 			mCurStep = STEP_TURN;
 			mLastUpdateTime = time;
 			gMotorDrive.drive(100, -100);
-			//gMultiServo.start(STABI_BASE_ANGLE);	//�X�^�r�L�΂�
 		}
 		break;
 	}
@@ -301,17 +243,9 @@ bool Waking::onInit(const struct timespec& time)
 	mCurStep = STEP_CHECK_LIE;
 
 	gMotorDrive.setRunMode(true);
-	//gGyroSensor.setRunMode(true);
-	//gAccelerationSensor.setRunMode(true);
-	//gJohnServo.setRunMode(true);
 	gMultiServo.setRunMode(true);
-	//gArmServo.setRunMode(true);
-	//gNeckServo.setRunMode(true);
 	gMultiServo.start(BACK_STABI_RUN_ANGLE);
 	gNineAxisSensor.setRunMode(true);
-	//gSServo.setRunMode(true);
-	//gSServo.moveRun();
-	//gPoseDetecting.setRunMode(true);
 	mWakeRetryCount = 0;
 	mLastUpdateTime = time;
 	gMotorDrive.drive(-100);
@@ -411,182 +345,9 @@ Waking::Waking() : mWakeRetryCount(0), mStartPower(45), mAngleThreshold(70), mDe
 Waking::~Waking()
 {
 }
-/*
-bool WakingFromLie::onInit(const struct timespec& time)
-{
-	mLastUpdateTime = time;
-	mLastDtTime = time;
-	mCurStep = STEP_FORWARD;
-	mCurrentPower = 0;
-	mNotLieCount = 0;
 
-	gMotorDrive.setRunMode(true);
-	//gSServo.setRunMode(true);
-	//gSServo.start(0, 0);
-	//gPoseDetecting.setRunMode(true);
-	mWakeRetryCount = 0;
 
-	return true;
-}
-void WakingFromLie::onUpdate(const struct timespec& time)
-{
-	double dt = Time::dt(time, mLastDtTime);
-	mLastDtTime = time;
 
-	switch (mCurStep)
-	{
-	case STEP_FORWARD:
-		
-		if (!gPoseDetecting.isLie())mNotLieCount++;
-		else mNotLieCount = 0;
-	
-		gMotorDrive.drive(mCurrentPower, mCurrentPower);
-
-		if (mNotLieCount > 100 || mCurrentPower >= MOTOR_MAX_POWER)
-		{
-			gMotorDrive.drive(0, 0);
-			mCurStep = STEP_VERIFY;
-			mLastUpdateTime = time;
-		}
-		mCurrentPower += MOTOR_MAX_POWER * dt * (WAKING_RETRY_COUNT + 1 - mWakeRetryCount) / (WAKING_RETRY_COUNT + 1) / mShortestSpeedUpPeriod;
-
-		break;
-	case STEP_VERIFY:
-		if (Time::dt(time, mLastUpdateTime) > 3)
-		{
-			if (!gPoseDetecting.isLie())
-			{
-				Debug::print(LOG_SUMMARY, "WakingFromLie: Successed waking!!\r\n");
-				gBuzzer.start(30, 20, 4);
-				setRunMode(false);
-				return;
-			}
-
-			if (mWakeRetryCount++ >= WAKING_RETRY_COUNT)
-			{
-				setRunMode(false);
-				return;
-			}
-			Debug::print(LOG_SUMMARY, "WakingFromLie: Retrying (%d/%d)\r\n", mWakeRetryCount, WAKING_RETRY_COUNT);
-			gMotorDrive.drive(MOTOR_MAX_POWER, MOTOR_MAX_POWER);
-			mCurStep = STEP_FORWARD;
-			mLastUpdateTime = time;
-			mCurrentPower = 0;
-		}
-		break;
-	}
-}
-bool WakingFromLie::onCommand(const std::vector<std::string>& args)
-{
-	if (args.size() == 2)
-	{
-		mShortestSpeedUpPeriod = atof(args[1].c_str());
-		Debug::print(LOG_SUMMARY, "Command executed!\r\n");
-		return true;
-	}
-	Debug::print(LOG_SUMMARY, "waking [period]: set speed up period\r\n");
-	return true;
-}
-void WakingFromLie::onClean()
-{
-	gMotorDrive.drive(0);
-	//gSServo.moveRun();
-}
-WakingFromLie::WakingFromLie() : mShortestSpeedUpPeriod(10)
-{
-	setName("wakinglie");
-	setPriority(TASK_PRIORITY_SEQUENCE, TASK_INTERVAL_SEQUENCE);
-}
-WakingFromLie::~WakingFromLie()
-{
-}
-*/
-
-/*
-bool Turning::onInit(const struct timespec& time)
-{
-	mTurnPower = 0;
-	//gGyroSensor.setRunMode(true);
-	mAngle=gNineAxisSensor.getRz();//gGyroSensor.getRz();
-	mLastUpdateTime = time;
-	return true;
-}
-void Turning::onUpdate(const struct timespec& time)
-{
-	//double turnedAngle = abs(GyroSensor::normalize(gGyroSensor.getRz() - mAngle));
-	double turnedAngle = abs(GyroSensor::normalize(gGyroSensor.getRz() - mAngle));
-	if (Time::dt(time, mLastUpdateTime) >= 5 || turnedAngle > 15)
-	{
-		Debug::print(LOG_SUMMARY, "Turning: Detected turning\r\n");
-		gMotorDrive.drive(0);
-		setRunMode(false);
-	}
-	else
-	{
-		if (mIsTurningLeft)gMotorDrive.drive(-mTurnPower, mTurnPower);
-		else gMotorDrive.drive(mTurnPower, -mTurnPower);
-		if (turnedAngle < 5)mTurnPower += 0.1;
-	}
-}
-void Turning::setDirection(bool left)
-{
-	mIsTurningLeft = left;
-}
-Turning::Turning()
-{
-	setName("turning");
-	setPriority(TASK_PRIORITY_SEQUENCE, TASK_INTERVAL_SEQUENCE);
-}
-Turning::~Turning()
-{
-}
-*/
-
-/*
-bool Avoiding::onInit(const struct timespec& time)
-{
-	mLastUpdateTime = time;
-	if (!gEscapingState.isActive())gMotorDrive.drive(0, 50);
-	mAngle = gGyroSensor.getRz();
-	mCurStep = STEP_TURN;
-	return true;
-}
-void Avoiding::onUpdate(const struct timespec& time)
-{
-	if (gEscapingState.isActive())
-	{
-		Debug::print(LOG_SUMMARY, "Avoiding: Escaping is already running. Avoiding Canceled!\r\n");
-		setRunMode(false);
-	}
-	switch (mCurStep)
-	{
-	case STEP_TURN:
-		if (Time::dt(time, mLastUpdateTime) > 5 || abs(GyroSensor::normalize(gGyroSensor.getRz() - mAngle)) > 45)
-		{
-			Debug::print(LOG_SUMMARY, "Avoiding: forwarding\r\n");
-			mLastUpdateTime = time;
-			gMotorDrive.drivePIDGyro(10, MOTOR_MAX_POWER, true);
-			mCurStep = STEP_FORWARD;
-		}
-		break;
-	case STEP_FORWARD:
-		if (Time::dt(time, mLastUpdateTime) > 4)
-		{
-			Debug::print(LOG_SUMMARY, "Avoiding: finished\r\n");
-			setRunMode(false);
-		}
-		break;
-	}
-}
-Avoiding::Avoiding()
-{
-	setName("avoiding");
-	setPriority(TASK_PRIORITY_SEQUENCE, TASK_INTERVAL_SEQUENCE);
-}
-Avoiding::~Avoiding()
-{
-}
-*/
 
 bool SensorLogging::onInit(const struct timespec& time)
 {
