19a5cf97de51131b85df8fbe75671c1c7da4a90e
KazumaMatsumoto <kazuma.m@uec.ac.jp>
2017-08-13 21:29:42 +0900

    randomコマンド受付

--- subsidiary_sequence.cpp
+++ subsidiary_sequence.cpp
@@ -12,117 +12,10 @@
 #include "actuator.h"
 #include "motor.h"
 
-Escaping gEscapingState;
 EscapingRandom gEscapingRandomState;
 EscapingByStabi gEscapingByStabiState;
 SensorLogging gSensorLoggingState;
 
-
-bool Escaping::onInit(const struct timespec& time)
-{
-	mLastUpdateTime = time;
-	mCurStep = STEP_BACKWARD;
-	gMotorDrive.drive(-100);
-	mEscapingTriedCount = 0;
-	return true;
-}
-void Escaping::onClean()
-{
-	;
-}
-void Escaping::onUpdate(const struct timespec& time)
-{
-  const static unsigned int ESCAPING_MAX_CAMERA_ESCAPING_COUNT = 20;
-  const static unsigned int ESCAPING_MAX_RANDOM_ESCAPING_COUNT = 20;
-  switch (mCurStep)
-    {
-    case STEP_BACKWARD:
-      //�o�b�N���s��
-      if (Time::dt(time, mLastUpdateTime) >= 2)
-	{
-	  Debug::print(LOG_SUMMARY, "Escaping: Backward finished!\r\n");
-	  mCurStep = STEP_AFTER_BACKWARD;
-	  mLastUpdateTime = time;
-	  gMotorDrive.drive(0);
-	}
-      break;
-    case STEP_AFTER_BACKWARD:
-      //�ċN���h�~�̂��ߑҋ@
-      if (Time::dt(time, mLastUpdateTime) >= 3)
-	{
-	  if (mEscapingTriedCount > ESCAPING_MAX_CAMERA_ESCAPING_COUNT)
-	    {
-	      //�����_���ڍs
-	      Debug::print(LOG_SUMMARY, "Escaping: aborting camera escape!\r\n");
-	      mEscapingTriedCount = 0;
-	      mCurStep = STEP_RANDOM;
-	      mCurRandomStep = RANDOM_STEP_FORWARD;
-	      break;
-	    }
-	  mLastUpdateTime = time;
-	}
-  break;
- case STEP_PRE_CAMERA:
-   //�摜�B�e�p�ɋN���オ�蓮�����s���A���b�ҋ@����
-   if (Time::dt(time, mLastUpdateTime) > 2)//�N���オ�芮�����A���莞�Ԃ��o�߂��Ă�����
-     {
-       Debug::print(LOG_SUMMARY, "Escaping: camera warming...\r\n");
-       //�摜�B�e�������s��
-       mCurStep = STEP_CAMERA;
-       mLastUpdateTime = time;
-       
-       
-       gMotorDrive.drive(0);
-     }
-   break;
-   
- case STEP_CAMERA_TURN:
-		break;
- case STEP_CAMERA_FORWARD:
-		//�摜�����̌��ʁA���i�����K�v���������ꍇ
-   if (Time::dt(time, mLastUpdateTime) >= 10)
-     {
-			gMotorDrive.drive(-100);
-			mCurStep = STEP_BACKWARD;
-			mLastUpdateTime = time;
-		}
-		break;
-	case STEP_CAMERA_TURN_HERE:
-		break;
-	}
-}
-void Escaping::stuckMoveRandom()
-{
-	switch (mCurRandomStep)
-	{
-	case RANDOM_STEP_BACKWARD:
-		//�o�b�N���s��
-		Debug::print(LOG_SUMMARY, "Escaping(random): backward\r\n");
-		mCurRandomStep = RANDOM_STEP_TURN;
-		gMotorDrive.drive(100, -100);
-		break;
-	case RANDOM_STEP_TURN:
-		//���̏����]���s��
-		Debug::print(LOG_SUMMARY, "Escaping(random): turning\r\n");
-		mCurRandomStep = RANDOM_STEP_FORWARD;
-		gMotorDrive.drive(100);
-		break;
-	case RANDOM_STEP_FORWARD:
-		//�O�i���s��
-		Debug::print(LOG_SUMMARY, "Escaping(random): forward\r\n");
-		mCurRandomStep = RANDOM_STEP_BACKWARD;
-		gMotorDrive.drive(-100);
-		break;
-	}
-}
-Escaping::Escaping()
-{
-	setName("escaping");
-	setPriority(TASK_PRIORITY_SEQUENCE, TASK_INTERVAL_SEQUENCE);
-}
-Escaping::~Escaping()
-{
-}
 
 bool EscapingByStabi::onInit(const struct timespec& time)
 {
@@ -215,21 +108,18 @@ void EscapingRandom::onUpdate(const struct timespec& time)
 
 		//出力を選ぶ
 		int stabiswitch = rand() % 2;//スタビ
-		if (RandomCount < 2){
-			//0,1回目は左右一斉運動
-			int motordirection = pow(-1, rand() % 2);
-			motorforce0 = rand() % 100 * motordirection;
+		if (RandomCount < 5){
+			//0-4回目は左右一斉運動
+			motorforce0 = ((rand() % 50)+50) * pow(-1, rand() % 2);
 			motorforce1 = motorforce0;
 		}
-		else if (RandomCount < 5) {
-			//2,3,4回目はタイヤ左右別々方向出力
-			int motordirection0 = pow(-1, rand() % 2);
-			int motordirection1 = motordirection0 * (-1);
-			motorforce0 = rand() % 100 * motordirection0;
-			motorforce1 = rand() % 100 * motordirection1;
+		else{
+			//5-9回目はタイヤ左右別々方向出力
+			motorforce0 = rand() % 100 * pow(-1, rand() % 2);
+			motorforce1 = rand() % 100 * pow(-1, rand() % 2);
 		}
 
-		//出力を挙げる
+		//出力を上げる
 		gMultiServo.start(stabiswitch);
 		gMotorDrive.drive(motorforce0, motorforce1);
 
@@ -238,32 +128,32 @@ void EscapingRandom::onUpdate(const struct timespec& time)
 		RandomCount++;//カウント	
 	}
 
-	if (RandomCount > 4) {
+	if (RandomCount > 10) {
 		gEscapingRandomState.setRunMode(false);
 		gEscapingByStabiState.setRunMode(true);
 	}
 
-	//switch (mCurStep)
-	//{
-	//case STEP_TURN:
-	//	//���̏����]���s��
-	//	if (Time::dt(time, mLastUpdateTime) >= 5)
-	//	{
-	//		mCurStep = STEP_FORWARD;
-	//		mLastUpdateTime = time;
-	//		gMotorDrive.drive(100);
-	//	}
-	//	break;
-	//case STEP_FORWARD:
-	//	//�O�i���s��
-	//	if (Time::dt(time, mLastUpdateTime) >= 3)
-	//	{
-	//		mCurStep = STEP_TURN;
-	//		mLastUpdateTime = time;
-	//		gMotorDrive.drive(100, -100);
-	//	}
-	//	break;
-	//}
+
+}
+bool EscapingRandom::onCommand(const std::vector<std::string>& args)
+{
+  if (args.size() == 2)
+  {
+    if (args[1].compare("start") == 0)
+    {
+      gEscapingRandomState.setRunMode(true);
+      return true;
+    }
+    if (args[1].compare("s") == 0)
+    {
+      gMotorDrive.drive(0);
+      Debug::print(LOG_SUMMARY, "Escaping Random Finished!\r\n");
+      gEscapingRandomState.setRunMode(false);
+      return true;
+    }
+  }
+  Debug::print(LOG_SUMMARY, "random start       : start Escaping by stabi mode\r\n\random s        : stop  Escaping by stabi mode\r\n");
+  return false;
 }
 EscapingRandom::EscapingRandom()
 {
--- subsidiary_sequence.h
+++ subsidiary_sequence.h
@@ -4,46 +4,6 @@
 #include "task.h"
 #include "utils.h"
 
-//�Q���O���m����
-class WadachiPredicting : public TaskBase
-{
-	struct timespec mLastUpdateTime;//�O���̃`�F�b�N����
-	bool mIsAvoidingEnable;
-	enum STEP{ STEP_RUNNING, STEP_STOPPING, STEP_WAKING, STEP_CHECKING, STEP_AVOIDING };
-	enum STEP mCurStep;
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-	virtual bool onCommand(const std::vector<std::string>& args);
-public:
-	bool isWorking(const struct timespec& time);//���O���m���쒆���ۂ�
-	WadachiPredicting();
-	~WadachiPredicting();
-};
-
-//�Q�E�o����
-//���̃^�X�N���L���̊Ԃ̓i�r�Q�[�V�������܂���
-class Escaping : public TaskBase
-{
-	struct timespec mLastUpdateTime;//�O���̍s�������̕ω�����
-
-	enum STEP{ STEP_BACKWARD = 0, STEP_AFTER_BACKWARD, STEP_PRE_CAMERA, STEP_CAMERA, STEP_CAMERA_TURN, STEP_CAMERA_FORWARD, STEP_CAMERA_TURN_HERE, STEP_RANDOM };
-	enum STEP mCurStep;
-	enum RANDOM_STEP{ RANDOM_STEP_BACKWARD = 0, RANDOM_STEP_TURN, RANDOM_STEP_FORWARD };
-	enum RANDOM_STEP mCurRandomStep;
-	unsigned int mEscapingTriedCount;//�J�����E�o�����s��������
-	double mAngle;
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onClean();
-	virtual void onUpdate(const struct timespec& time);
-
-	void stuckMoveRandom();//�X�^�b�N���̈ړ�����
-	//	void stuckMoveCamera(IplImage* pImage);//�J�������p�����X�^�b�N���̈ړ�����
-public:
-	Escaping();
-	~Escaping();
-};
 
 //�Q�E�o�E�o�i�X�^�r�g�pver�j
 class EscapingByStabi : public TaskBase
@@ -72,6 +32,7 @@ class EscapingRandom : public TaskBase
 protected:
 	virtual bool onInit(const struct timespec& time);
 	virtual void onUpdate(const struct timespec& time);
+  virtual bool onCommand(const std::vector<std::string>& args);
 	int motorforce0;
 	int motorforce1;
 public:
@@ -80,85 +41,7 @@ public:
 };
 
 
-//���[�o�[�̎p������
-//�p�����䂪���������ƃ^�X�N���I�����܂�
-/*
-class WakingFromLie : public TaskBase
-{
-	struct timespec mLastUpdateTime;
-	struct timespec mLastDtTime;
-	enum STEP{STEP_FORWARD, STEP_VERIFY};
-	enum STEP mCurStep;
-	unsigned int mWakeRetryCount;
-	double mShortestSpeedUpPeriod;
-	double mCurrentPower;
-	unsigned int mNotLieCount;
 
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-	virtual bool onCommand(const std::vector<std::string>& args);
-	virtual void onClean();
-public:
-	WakingFromLie();
-	~WakingFromLie();
-};
-*/
-
-//���[�o�[�̂��̏����]
-//���������ƃ^�X�N���I�����܂�
-/*
-class Turning : public TaskBase
-{
-	bool mIsTurningLeft;
-	double mTurnPower;
-	double mAngle;
-	struct timespec mLastUpdateTime;//�s���J�n����
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-public:
-	void setDirection(bool left);
-
-	Turning();
-	~Turning();
-};
-*/
-
-//�Q���O���m���̉��𓮍�
-//���������ƃ^�X�N���I�����܂�
-/*
-class Avoiding : public TaskBase
-{
-	struct timespec mLastUpdateTime;//�s���J�n����
-	double mAngle;
-	enum STEP { STEP_TURN = 0, STEP_FORWARD };
-	enum STEP mCurStep;
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-public:
-
-	Avoiding();
-	~Avoiding();
-};
-*/
-
-/*
-//�L�O�B�e
-class PictureTaking : public TaskBase
-{
-	struct timespec mLastUpdateTime;
-	unsigned int mStepCount;
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-public:
-	PictureTaking();
-	~PictureTaking();
-};
-*/
-//�Z���T�[���O
 class SensorLogging : public TaskBase
 {
 	struct timespec mLastUpdateTime;
@@ -174,38 +57,6 @@ public:
 	~SensorLogging();
 };
 
-
-class EncoderMonitoring : public TaskBase
-{
-	struct timespec mLastSamplingTime;			//�p���X�����T���v�����O�����������ۑ�
-	struct timespec mLastUpdateTime;			//臒l���X�V�����������ۑ�
-
-	long long mStoredPulse;			//���莞�ԓ��̃p���X�ő��l(������臒l�Ɏg�p)
-	long long mCurrentMaxPulse;		//���݂̊��ԓ��̃p���X�ő��l
-	long long mPrevDeltaPulseL, mPrevDeltaPulseR;//�O���̃p���X��
-
-	int mUpdateTimer;					//臒l���X�V�����Ԋu(�b)
-	long long mThresholdPulse;			//mStoredPulse���炱�̒l���������l��臒l�ɂȂ�
-	long long mIgnoredDeltaUpperPulse;	//���̒l���ȏ��p���X���������ꍇ��臒l���X�V���Ȃ�
-	long long mIgnoredDeltaLowerPulse;	//���̒l���ȏ��p���X���������ꍇ��臒l���X�V���Ȃ�
-	long long mUpperThreshold;			//臒l�̏���
-	long long mLowerThreshold;			//臒l�̉���
-	bool mIsPrint;								//true�Ȃ�1�b���ƂɃp���X�����\������
-protected:
-	virtual bool onInit(const struct timespec& time);
-	virtual void onUpdate(const struct timespec& time);
-	virtual bool onCommand(const std::vector<std::string>& args);
-
-	//臒l���X�V����
-	virtual void updateThreshold();
-	virtual bool removeError(long long pulseL, long long pulseR);					//�p���X���ُ̈��l������ true: �ُ��l�����o
-public:
-	EncoderMonitoring();
-	~EncoderMonitoring();
-};
-
-
-extern Escaping gEscapingState;
 extern EscapingRandom gEscapingRandomState;
 extern EscapingByStabi gEscapingByStabiState;
 extern SensorLogging gSensorLoggingState;
