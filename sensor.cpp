a18ffdbddf86957c59b5fba339a96ae054b8e814
mulcheese_developer <takadamalab@gmail.com>
2017-08-14 07:00:45 +0000

    fix:goal threshold.  remove:gps mod

--- sensor.cpp
+++ sensor.cpp
@@ -207,7 +207,6 @@ void GPSSensor::onUpdate(const struct timespec& time)
 	}
 	else 
   {
-    if(mPos.x != newdata->fix.latitude && mPos.y != newdata->fix.longitude && mPos.z != newdata -> fix.altitude)
     {
   		mPos.x = newdata->fix.latitude;
     	mPos.y = newdata->fix.longitude;
