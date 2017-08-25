a706407f5746e2bc799be43ecbc4801b33c58db6
f1410115@edu.cc.uec.ac.jp <f1410115@edu.cc.uec.ac.jp>
2017-08-25 21:41:46 +0900

    Constants::を全部に着けた

--- actuator.cpp
+++ actuator.cpp
@@ -151,15 +151,15 @@ Buzzer::~Buzzer()
 // フルカラーLED
 //////////////////////////////
 bool LED::onInit(const struct timespec& time) {
-	pinMode(PIN_LED_R, PWM_OUTPUT);
-	pinMode(PIN_LED_G, PWM_OUTPUT);
-	pinMode(PIN_LED_B, PWM_OUTPUT);
-	softPwmCreate(PIN_LED_R, 0, 256);
-	softPwmCreate(PIN_LED_G, 0, 256);
-	softPwmCreate(PIN_LED_B, 0, 256);
-	digitalWrite(PIN_LED_R, LOW);
-	digitalWrite(PIN_LED_G, LOW);
-	digitalWrite(PIN_LED_B, LOW);
+	pinMode(Constants::PIN_LED_R, PWM_OUTPUT);
+	pinMode(Constants::PIN_LED_G, PWM_OUTPUT);
+	pinMode(Constants::PIN_LED_B, PWM_OUTPUT);
+	softPwmCreate(Constants::PIN_LED_R, 0, 256);
+	softPwmCreate(Constants::PIN_LED_G, 0, 256);
+	softPwmCreate(Constants::PIN_LED_B, 0, 256);
+	digitalWrite(Constants::PIN_LED_R, LOW);
+	digitalWrite(Constants::PIN_LED_G, LOW);
+	digitalWrite(Constants::PIN_LED_B, LOW);
 	mLastUpdateTime1 = mLastUpdateTime2 = time;
 	r = g = b = t = s = u = v = p = d = 0;
 	rbw = bnk = hf = false;
@@ -167,9 +167,9 @@ bool LED::onInit(const struct timespec& time) {
 }
 
 void LED::onClean() {
-	digitalWrite(PIN_LED_R, LOW);
-	digitalWrite(PIN_LED_G, LOW);
-	digitalWrite(PIN_LED_B, LOW);
+	digitalWrite(Constants::PIN_LED_R, LOW);
+	digitalWrite(Constants::PIN_LED_G, LOW);
+	digitalWrite(Constants::PIN_LED_B, LOW);
 	rbw = false;
 }
 
@@ -270,9 +270,9 @@ bool LED::onCommand(const std::vector<std::string>& args) {
 }
 
 void LED::reflect() {
-	softPwmWrite(PIN_LED_R, r);
-	softPwmWrite(PIN_LED_G, g);
-	softPwmWrite(PIN_LED_B, b);
+	softPwmWrite(Constants::PIN_LED_R, r);
+	softPwmWrite(Constants::PIN_LED_G, g);
+	softPwmWrite(Constants::PIN_LED_B, b);
 }
 
 void LED::setColor(int x) {
@@ -291,9 +291,9 @@ void LED::setColor(int c1, int c2, int c3) {
 
 
 void LED::turnOff() {
-	softPwmWrite(PIN_LED_R, 0);
-	softPwmWrite(PIN_LED_G, 0);
-	softPwmWrite(PIN_LED_B, 0);
+	softPwmWrite(Constants::PIN_LED_R, 0);
+	softPwmWrite(Constants::PIN_LED_G, 0);
+	softPwmWrite(Constants::PIN_LED_B, 0);
 }
 
 
